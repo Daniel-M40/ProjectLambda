24e1d0d9c64c19cd7bf71608b7ad5decdd22973e
Buck995 <h.buck995@gmail.com>
2023-11-15 15:06:42 +0000

    Setup for AI

---
+++ Source/ProjectLambda/AI/ChaseAIController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChaseAIController.h"
+
+AChaseAIController::AChaseAIController()
+{
+}
+
+void AChaseAIController::OnPossess(APawn* InPawn)
+{
+}
