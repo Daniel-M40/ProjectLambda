65787502db18c1f1e8ee806002897188a3fb791d
Daniel-M40 <daniel.moir0993@gmail.com>
2023-11-19 17:03:04 +0000

    Created Shotgun class

---
+++ Source/ProjectLambda/Player/Weapons/Shotgun/Shotgun.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Shotgun.h"
+
+#include "ProjectLambda/Player/Projectiles/Projectile.h"
+
+
+// Sets default values
+AShotgun::AShotgun()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AShotgun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShotgun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AShotgun::Fire()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Shotgun Fired"));
+
+	//If we have the projectile class and can fire spawn projectile
+	if (ProjectileClass && bCanFire) // Checks the projectile has been set in the editor
+	{
+		for (int32 Index = 0; Index < NumProjectiles; Index++)
+		{
+			FVector SpawnLoacation = ProjectileSpawn->GetComponentLocation();
+			FRotator SpawnRotation = ProjectileSpawn->GetComponentRotation();
+
+			//Spawn projectile at projectile spawn location and rotation
+			AProjectile* Bullet = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, SpawnLoacation, SpawnRotation);
+
+			// Calculate spread angle and adjust projectile rotation here
+			float SpreadAngle = ((float)Index / (float)(NumProjectiles - 1)) * ConeSpreadAngle - (ConeSpreadAngle / 2.0f);
+			FRotator SpreadRotation = GetActorRotation() + FRotator(0.0f, SpreadAngle, 0.0f);
+
+			Bullet->SetActorRotation(SpreadRotation);
+
+			//Set the owner of the projectile
+			Bullet->SetOwner(this);
+		}
+		
+
+
+		EnableFireTimer();
+	}
+}
+
---
+++ Source/ProjectLambda/Player/Weapons/Shotgun/Shotgun.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectLambda/Player/Weapons/WeaponBase.h"
+#include "Shotgun.generated.h"
+
+UCLASS()
+class PROJECTLAMBDA_API AShotgun : public AWeaponBase
+{
+	GENERATED_BODY()
+
+#pragma region Properties
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Shotgun")
+	int32 NumProjectiles = 3;
+
+	UPROPERTY(EditAnywhere, Category = "Shotgun")
+	float ConeSpreadAngle = 20.f;
+
+#pragma endregion
+
+#pragma region Actions
+	
+public:
+	// Sets default values for this actor's properties
+	AShotgun();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Fire() override;
+
+#pragma endregion
+};
