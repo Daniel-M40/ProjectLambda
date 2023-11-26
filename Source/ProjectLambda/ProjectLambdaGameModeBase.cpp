7fb3bbe90f024ab129932234c913c558fb46aaba
Daniel-M40 <daniel.moir0993@gmail.com>
2023-11-26 08:35:18 +0000

    ProjectLambdaGameMode - tracks players time

--- Source/ProjectLambda/ProjectLambdaGameModeBase.cpp
+++ Source/ProjectLambda/ProjectLambdaGameModeBase.cpp
@@ -3,3 +3,19 @@
 
 #include "ProjectLambdaGameModeBase.h"
 
+#include "Kismet/KismetStringLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void AProjectLambdaGameModeBase::StartTimer()
+{
+	GetWorldTimerManager().SetTimer(TimerHandle, this, &IncrementTimer, 1.f, true);
+}
+
+void AProjectLambdaGameModeBase::IncrementTimer()
+{
+	//Get current game time in seconds, also effected when game is paused and slo-mo
+	PlayerTime = UKismetSystemLibrary::GetGameTimeInSeconds(this);
+
+	//Convert the time in seconds to string to display
+	TimerTxt = UKismetStringLibrary::TimeSecondsToString(PlayerTime);
+}
--- Source/ProjectLambda/ProjectLambdaGameModeBase.h
+++ Source/ProjectLambda/ProjectLambdaGameModeBase.h
@@ -13,5 +13,34 @@ UCLASS()
 class PROJECTLAMBDA_API AProjectLambdaGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+#pragma region Properties
+private:
+	//Track how quickly the player finishes the game
+	double PlayerTime = 0;
+	
+	FTimerHandle TimerHandle;
+
+public:
+	//Stores time as text
+	UPROPERTY(BlueprintReadOnly, Category="")
+	FString TimerTxt = "";
+#pragma endregion
+
+	
+#pragma region Actions
+	
+private:
+	//Starts timer when game starts
+	void StartTimer();
+
+	void IncrementTimer();
+
+	//Starts timer when player finishes the game
+	void EndTimer();
+
+#pragma endregion
+
+
 	
 };
