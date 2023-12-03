d60ed92ac43ebbdcc7b891049c6c69b1319e1ec1
reeba-0-0 <ariba22022@hotmail.com>
2023-12-03 18:15:30 +0000

    Pickup component
    Created pickup component which increases health by 10 and coins by 20 (for now). Also made them spawn randomly around the game but that can be changed if we're doing it differently. Currency UI is on too

--- Source/ProjectLambda/ProjectLambdaGameModeBase.h
+++ Source/ProjectLambda/ProjectLambdaGameModeBase.h
@@ -13,42 +13,7 @@ UCLASS()
 class PROJECTLAMBDA_API AProjectLambdaGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-
-#pragma region Properties
-private:
 	
-	
-	FTimerHandle TimerHandle;
-
-public:
-
-	//Stores time as text
-	UPROPERTY(BlueprintReadOnly, Category="")
-	FString TimerTxt = "";
-
-	UPROPERTY(BlueprintReadOnly, Category = "")
-
-	//Track how quickly the player finishes the game
-	double PlayerTime = 0;
-
-#pragma endregion
-
-	
-#pragma region Actions
-	
-private:
-
-	////Track how quickly the player finishes the game
-	//double PlayerTime = 0;
-
-	//Starts timer when game starts
-	void StartTimer();
-	void IncrementTimer();
-
-	//Starts timer when player finishes the game
-	void EndTimer();
-
-#pragma endregion
 
 
 	
