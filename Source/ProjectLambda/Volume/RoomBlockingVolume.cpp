4fb18884e4d2927d19fa98b5c10a4091e83c3891
Daniel-M40 <daniel.moir0993@gmail.com>
2023-12-03 10:16:49 +0000

    created room blocking volume

---
+++ Source/ProjectLambda/Volume/RoomBlockingVolume.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RoomBlockingVolume.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+ARoomBlockingVolume::ARoomBlockingVolume()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Create box component default components
+	OverlapVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("BlockingVolume"));
+
+	//Set the box component attachment to the root component.
+	OverlapVolume->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ARoomBlockingVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARoomBlockingVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
